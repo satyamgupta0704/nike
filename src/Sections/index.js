d7148f325f40ff83ca4af156dbf6550604ae9adf
satyamgupta0704 <21bcs7408@cuchd.in>
2024-07-12 00:42:38 +0530

    made components

---
+++ src/Sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export{
+    CustomerReviews,
+    Footer,
+    Hero,
+    Nav,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SuperQuality
+}
