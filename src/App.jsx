a0c655784ee9b8e8ffb9d8252f76f6aeb62c00ef
satyamgupta0704 <21bcs7408@cuchd.in>
2024-07-11 18:15:41 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = ()=>(
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+  </main>
+);
+export default App;
